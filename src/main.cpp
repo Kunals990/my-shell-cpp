2e5582ae7ed287ca36fc5b792db0c83920d963d1
Kunals990 <kunalsable9990@gmail.com>
2025-06-26 17:00:39 +0530

    Add type command

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include <string.h>
 #include <string>
 
@@ -13,14 +14,27 @@ int main() {
 
       std::string input;
       std::getline(std::cin, input);
+      std::pmr::map<std::string,int>mpp;
+      mpp["echo"]=1;
+      mpp["exit"]=2;
       if (input =="exit 0") {
           return 0;
       }
       if (input.substr(0,5)=="echo ") {
           std::cout<<input.substr(5)<<std::endl;
-          continue;
       }
-      std::cout << input <<": command not found"<<std::endl;
+      else if (input.substr(0,5)=="type ") {
+          std::string command = input.substr(5);
+          if (mpp.find(command) != mpp.end()) {
+              std::cout<<command<<" is a shell builtin"<<std::endl;
+          }
+          else {
+              std::cout<<command<<": not found"<<std::endl;
+          }
 
+      }
+      else {
+          std::cout << input <<": command not found"<<std::endl;
+      }
   }
 }
